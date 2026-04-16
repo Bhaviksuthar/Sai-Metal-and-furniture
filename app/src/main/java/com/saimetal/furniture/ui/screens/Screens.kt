3443a95d562901435d5041860f796ab589f14b1b
bhavik <bhaviks1224@gmail.com>
2026-04-16 22:16:32 +0530

    Fix screen syntax and Compose row weight import

--- app/src/main/java/com/saimetal/furniture/ui/screens/Screens.kt
+++ app/src/main/java/com/saimetal/furniture/ui/screens/Screens.kt
@@ -14,8 +14,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -712,7 +712,7 @@ fun AdminBillingScreen(viewModel: SaiMetalViewModel, modifier: Modifier = Modifi
                     Text(option)
                 }
             }
-        )
+        }
         OutlinedTextField(
             value = viewModel.billingDraft.dueAmount,
             onValueChange = { viewModel.updateBillingDraft { copy(dueAmount = it) } },
