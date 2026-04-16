839ea1fb90ca89f38d9b24fa23d8014d4f2996dc
bhavik <bhaviks1224@gmail.com>
2026-04-16 20:33:16 +0530

    Avoid OneDrive locking Gradle build outputs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
+
+val localAppData = System.getenv("LOCALAPPDATA")
+if (!localAppData.isNullOrBlank()) {
+    rootProject.layout.buildDirectory.set(file("$localAppData/SaiMetalFurniture/build/root"))
+    subprojects {
+        layout.buildDirectory.set(file("$localAppData/SaiMetalFurniture/build/$name"))
+    }
+}
